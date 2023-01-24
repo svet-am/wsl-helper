5a213f9c8ea890a5812e28ffb3c670b615780bea
Tony McDowell <svet.am@gmail.com>
2023-01-24 15:37:28 -0700

    LICENSE: Add SPDX style license header to main script source

--- wsl-helper.ps1
+++ wsl-helper.ps1
@@ -1,5 +1,7 @@
 # wsl-helper.ps1
 # a helper script for installing and maintaining WSLv2 deployments
+# SPDX-License-Identifier: MIT
 # original author Tony McDowell <svet.am@gmail.com>
+
 
 write-host "Hello World"
