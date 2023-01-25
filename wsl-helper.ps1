d307d196e93e6f891120838b40c490e51ffa76e4
Tony McDowell <svet.am@gmail.com>
2023-01-24 18:16:48 -0700

    General: modularize with a menu function

--- wsl-helper.ps1
+++ wsl-helper.ps1
@@ -7,6 +7,21 @@
 $debugMode		= 1
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force;
 
+function PaintMenu {
+	do {
+		$menuSelection = -1
+		Write-Host "===================================="
+		Write-Host "1. Install WSLv2"
+		Write-Host "2. Expand WSLv2 Virtual Hard Disk"
+		Write-Host "3. Compact WSLv2 Virtual Hard Disk"
+		Write-Host "4. Install WSLv2 Distribution"
+		Write-Host "5. Exit"
+		Write-Host "===================================="
+		$menuSelection = Read-Host  -Prompt "Selection: "
+		
+	} while($menuSelection -le 0) 
+}
+
 function InstallIfNotInstalled {
     [CmdletBinding()]
     param(
@@ -20,40 +35,44 @@ function InstallIfNotInstalled {
     }
 }
 
-# Check if winget is installed
-# This code snippet courtesy of https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winutil.ps1
-Write-Host "Checking if Winget is Installed..."
-if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
-	#Checks if winget executable exists and if the Windows Version is 1809 or higher
-	Write-Host "Winget Already Installed"
-} else {
-		#Gets the computer's information
-		$ComputerInfo = Get-ComputerInfo
-		#Gets the Windows Edition
-		if ($ComputerInfo.OSName) {
-			$OSName = $ComputerInfo.OSName
-		} else {
-			$OSName = $ComputerInfo.WindowsProductName
-		}
-		if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
-						
-			Write-Host "Running Alternative Installer for LTSC/Server Editions"
+function InstallWinget {
+	# Check if winget is installed
+	# This code snippet courtesy of https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winutil.ps1
+	Write-Host "Checking if Winget is Installed..."
+	if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe) {
+		#Checks if winget executable exists and if the Windows Version is 1809 or higher
+		Write-Host "Winget Already Installed"
+	} else {
+			#Gets the computer's information
+			$ComputerInfo = Get-ComputerInfo
+			#Gets the Windows Edition
+			if ($ComputerInfo.OSName) {
+				$OSName = $ComputerInfo.OSName
+			} else {
+				$OSName = $ComputerInfo.WindowsProductName
+			}
+			if (((($OSName.IndexOf("LTSC")) -ne -1) -or ($OSName.IndexOf("Server") -ne -1)) -and (($ComputerInfo.WindowsVersion) -ge "1809")) {
+							
+				Write-Host "Running Alternative Installer for LTSC/Server Editions"
 
-			# Switching to winget-install from PSGallery from asheroto
-			# Source: https://github.com/asheroto/winget-installer
-						
-			Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal
-						
-		} elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
-			#Checks if Windows Version is too old for winget
-			Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
-		} else {
-			#Installing Winget from the Microsoft Store
-			Write-Host "Winget not found, installing it now."
-			Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
-			$nid = (Get-Process AppInstaller).Id
-			Wait-Process -Id $nid
-			Write-Host "Winget Installed"
-		}
-	
+				# Switching to winget-install from PSGallery from asheroto
+				# Source: https://github.com/asheroto/winget-installer
+							
+				Start-Process powershell.exe -Verb RunAs -ArgumentList "-command irm https://raw.githubusercontent.com/ChrisTitusTech/winutil/$BranchToUse/winget.ps1 | iex | Out-Host" -WindowStyle Normal
+							
+			} elseif (((Get-ComputerInfo).WindowsVersion) -lt "1809") {
+				#Checks if Windows Version is too old for winget
+				Write-Host "Winget is not supported on this version of Windows (Pre-1809)"
+			} else {
+				#Installing Winget from the Microsoft Store
+				Write-Host "Winget not found, installing it now."
+				Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
+				$nid = (Get-Process AppInstaller).Id
+				Wait-Process -Id $nid
+				Write-Host "Winget Installed"
+			}
+		
+	}
 }
+
+PaintMenu
